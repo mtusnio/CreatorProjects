e7ce99f2e8477b3874daaae0afb8d8890ba932d5
Michal Tusnio <m.tusnio@gmail.com>
2016-06-08 00:57:12 +0100

    Cleaned up files, added LED changes to the joystick file

---
+++ Makefile
@@ -0,0 +1,1 @@
+gcc -lletmecreate_core -lletmecreate_click -I /usr/include/letmecreate/ -std=c99 main.c
--- main.c
+++ main.c
@@ -1,22 +1,43 @@
 #include <stdio.h>
-#include "core/i2c.h"
-#include "click/common.h"
-#include "click/joystick.h"
+#include <core/common.h>
+#include <core/led.h>
+#include <core/i2c.h>
+#include <click/joystick.h>
 
+#define OFFSET 98
+#define MAXIMUM (OFFSET * 2)
+
+int resolve_percentage(float perc)
+{
+    int mask = 0;
+    int div = perc/0.14f;
+
+    for(int i = 0; i < div; i++)
+        mask |= (1 << i);
+
+    return mask;
+}
 
 int main()
 {
-    i2c_init(MIKROBUS_I2C_BUS_2);
-    i2c_select_bus(MIKROBUS_I2C_BUS_2);
-
+    i2c_init(MIKROBUS_2);
+    i2c_select_bus(MIKROBUS_2);
+    led_init();
     while(1)
     {
         int8_t x, y;
+        int mask;
 
         joystick_click_get_position(&x, &y);
         printf("%i %i\n", (int)x, (int)y);
+
+        mask = resolve_percentage((float)(x + OFFSET)/(float)MAXIMUM);
+        led_switch_on(mask);
+        led_switch_off(~mask);
     }
-    i2c_release(MIKROBUS_I2C_BUS_2);
+
+    led_release();
+    i2c_release(MIKROBUS_2);
 
     return 0;
 }
