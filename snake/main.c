06ccf16ba67ab5bc6e056ed76179c48f66584123
Michal Tusnio <Michal.Tusnio@imgtec.com>
2016-06-15 16:01:58 +0100

    Full Snake mechanics, score display at the end

--- snake/main.c
+++ snake/main.c
@@ -1,7 +1,9 @@
 #include <stdlib.h>
+#include <time.h>
 #include <unistd.h>
 #include <stdio.h>
 #include <linux/spi/spidev.h>
+#include <math.h>
 
 #include <core/common.h>
 #include <core/i2c.h>
@@ -9,10 +11,17 @@
 #include <click/joystick.h>
 #include <click/led_matrix.h>
 
-#define TOLERANCE 30
+#define GAME_SPEED 480000000
+
+#define BLINKS 3
+#define BLINK_SPEED 550000000
+
+#define TOLERANCE 25
 
 #define SIZE 8
-#define SNAKE_SIZE 8
+#define HALF_SIZE 4
+
+#define SNAKE_SIZE 16
 
 #define DIR_LEFT 0
 #define DIR_UP 1
@@ -25,18 +34,102 @@ typedef struct pos_t
 	int y;
 } Pos;
 
+int display_number(int number)
+{
+        int i;
+        int digits[2];
+
+        if(number < 0 || number > 99)
+                return -1;
+
+        uint8_t columns[10][HALF_SIZE] = {
+        { 60, 66, 66, 60 }, // 0
+        { 0, 126, 32, 16 }, // 1
+        { 50, 74, 70, 34 }, // 2
+        { 126, 90, 90, 66 }, // 3
+        { 8, 126, 40, 24 }, // 4
+        { 94, 82, 82, 114 }, // 5
+        { 94, 82, 82, 126}, // 6
+        { 126, 64, 64, 96 }, // 7
+        { 126, 90, 90, 126 }, // 8
+        { 126, 82, 82, 114 }, // 9
+	};
+
+        if(number >= 10)
+        {
+                digits[0] = number / 10;
+                digits[1] = number % 10;
+        }
+        else
+        {
+                digits[0] = 0;
+                digits[1] = number;
+        }
+
+        uint8_t display[SIZE];
+        for(i = 0; i < SIZE; i++)
+                display[i] = 0;
+
+        printf("Printing %i%i", digits[0], digits[1]);
+        for(i = 0; i < HALF_SIZE; i++)
+                display[i] = columns[digits[1]][i];
+
+        for(i = HALF_SIZE; i < SIZE; i++)
+                display[i] = columns[digits[0]][i - HALF_SIZE];
+
+        led_matrix_click_set(display);
+
+        return 0;
+
+}
+
+
+Pos get_random_fruit_position(Pos ** snake, int snake_length)
+{
+	// The naive way of doing it - just randomize until you find a
+	// position not occupied by the snake
+	while(1)
+	{
+		int i, x, y, valid;
+
+		valid = 1;
+		x = rand() % SIZE;
+		y = rand() % SIZE;
+
+		for(i = 0; i < snake_length; i++)
+		{
+			if(snake[i]->x == x &&
+				snake[i]->y == y)
+			{
+				valid = 0;
+				break;
+			}
+		}
+
+		if(valid)
+		{
+			Pos ret;
+			ret.x = x;
+			ret.y = y;
+			return ret;
+		}
+	}
+}
+
 int main()
 {
-	int i, length;
-	int direction = DIR_LEFT;
+	int i, length, score, direction, extend;
 	Pos * snake[SNAKE_SIZE];
+	Pos fruit;
+	uint8_t clear_columns[SIZE];
+	for(i = 0; i < SIZE; i++)
+		clear_columns[i] = 0;
 
-	for(i = 0; i < SNAKE_SIZE; i++)
-		snake[i] = 0;
+	srand(time(0));
 
 	if(i2c_init() < 0 || i2c_select_bus(MIKROBUS_1) < 0)
 		return -1;
-	
+
 	if(spi_init(MIKROBUS_2, SPI_MODE_3) < 0)
 		return -1;
 
@@ -46,21 +139,36 @@ int main()
 	if(led_matrix_click_enable() < 0)
 		return -1;
 
+	// Initialize game
+	for(i = 0; i < SNAKE_SIZE; i++)
+		snake[i] = 0;
+
 	snake[0] = malloc(sizeof(Pos));
 	snake[1] = malloc(sizeof(Pos));
+	snake[2] = malloc(sizeof(Pos));
 
-	snake[0]->x = 1;
+	snake[0]->x = 2;
 	snake[0]->y = 3;
-	
-	snake[1]->x = 0;
+
+	snake[1]->x = 1;
 	snake[1]->y = 3;
 
-	length = 2;
+	snake[2]->x = 0;
+	snake[2]->y = 3;
 
+	direction = DIR_LEFT;
+	length = 3;
+	score = 0;
+	extend = 0;
+
+	fruit = get_random_fruit_position(snake, length);
 	while (1)
 	{
 		// Controls
+		int collided;
 		int8_t joyX, joyY;
+		struct timespec t;
+
 		if(joystick_click_get_position(&joyX, &joyY) < 0)
 		{
 			printf("Failed to get joystick position");
@@ -69,21 +177,34 @@ int main()
 
 		if(abs(joyX) > TOLERANCE || abs(joyY) > TOLERANCE)
 		{
-			if(abs(joyX) > abs(joyY) && 
+			if(abs(joyX) > abs(joyY) &&
 			(direction != DIR_LEFT && direction != DIR_RIGHT))
 				direction = joyX > 0 ? DIR_LEFT : DIR_RIGHT;
 			else if(abs(joyY) > abs(joyX) &&
-			(direction != DIR_UP && direction != DIR_DOWN)) 
+			(direction != DIR_UP && direction != DIR_DOWN))
 				direction = joyY < 0 ? DIR_DOWN : DIR_UP;
 		}
 
 
 		// Logic
+		if(extend)
+		{
+			snake[length] = malloc(sizeof(Pos));
+			snake[length]->x = snake[length - 1]->x;
+			snake[length]->y = snake[length - 1]->y;
+		}
+
 		for(i = length - 1; i > 0; i--)
 		{
 			snake[i]->x = snake[i - 1]->x;
-			snake[i]->y = snake[i - 1]->y;	
-		}	
+			snake[i]->y = snake[i - 1]->y;
+		}
+
+		if(extend)
+		{
+			extend = 0;
+			length++;
+		}
 
 		switch(direction)
 		{
@@ -104,11 +225,20 @@ int main()
 			break;
 		}
 
+		if(fruit.x == snake[0]->x &&
+			fruit.y == snake[0]->y)
+		{
+			score++;
+			fruit = get_random_fruit_position(snake, length);
+			if(length < SNAKE_SIZE)
+				extend = 1;
+		}
+
 		// Draw
 		uint8_t columns[SIZE];
-
 		for(i = 0; i < SIZE; i++)
 			columns[i] = 0;
+
 		for(i = 0; i < length; i++)
 		{
 			int x = snake[i]->x;
@@ -117,15 +247,55 @@ int main()
 			columns[x] |= (1 << y);
 		}
 
-		led_matrix_click_set(columns);
-		sleep(1);
+		columns[fruit.x] |= (1 << fruit.y);
 
+		led_matrix_click_set(columns);
+
+		t.tv_sec = 0;
+		t.tv_nsec = GAME_SPEED/sqrt(score > 0 ? score : 1);
+		nanosleep(&t, 0);
+
+		// Collision detection
 		if(snake[0]->x < 0 || snake[0]->x >= SIZE)
-			break;
+			collided = 1;
 
 		if(snake[0]->y < 0 || snake[0]->y >= SIZE)
+			collided = 1;
+
+		for(i = 1; i < length; i++)
+		{
+			if(snake[i]->x == snake[0]->x &&
+				snake[i]->y == snake[0]->y)
+			{
+				collided = 1;
+				break;
+			}
+		}
+
+		if(collided)
+		{
+			t.tv_sec = 0;
+			t.tv_nsec = BLINK_SPEED;
+			nanosleep(&t, 0);
+
+			// Don't blink the fruit
+			clear_columns[fruit.x] |= (1 << fruit.y);
+			for(i = 0; i < BLINKS; i++)
+			{
+				led_matrix_click_set(clear_columns);
+				nanosleep(&t, 0);
+				led_matrix_click_set(columns);
+				nanosleep(&t, 0);
+			}
+
+			display_number(score);
+			sleep(5);
 			break;
+		}
+
 	}
+
+	printf("Score: %i\n", score);
 
 	for(i = 0; i < length; i++)
 		free(snake[i]);
